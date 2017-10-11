764b2051dac3ed0ce9a5d489ce0a57a36d17311f
Geoff Lange <geofflange@geoffs-macbook-pro.local>
2017-10-11 11:33:04 -0400

    Initial Commit

---
+++ GitExample2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExample2
+//
+//  Created by Geoff Lange on 10/11/17.
+//  Copyright © 2017 Geoff Lange. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitExample2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExample2
+//
+//  Created by Geoff Lange on 10/11/17.
+//  Copyright © 2017 Geoff Lange. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
